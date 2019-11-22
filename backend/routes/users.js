23e9d776ede237b04e066a1a6e1faa4276037304
bushbass <alex.nielsen@gmail.com>
2019-11-22 08:22:16 -0500

    udated all user crud routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// DELETE - show all users
+// ROUTE - /users/
 router.route('/').get((req, res) => {
   User.find()
     .then(users => {
@@ -10,6 +12,16 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// GET - show one user by ID
+// ROUTE - /users/:id
+router.route('/:id').get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// GET - add a user
+// ROUTE - /users/:id
 router.route('/add').post((req, res) => {
   const { username } = req.body;
   const newUser = new User({ username });
@@ -19,17 +31,26 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// POST - update user by ID
+// ROUTE - /users/:id
 router.route('/update/:id').post((req, res) => {
-  // const { username, description, duration, date } = req.body;
   User.findById(req.params.id)
+
     .then(user => {
       user.username = req.body.username;
-
       user
         .save()
-        .then(() => res.json('user updated!'))
+        .then(() => res.json(`user ${user.username} updated!`))
         .catch(err => res.status(400).json('Error: ' + err));
     })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// DELETE - delete one user by id
+// ROUTE - /users/:id
+router.route('/:id').delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(response => res.json(` User ${response.username} deleted`))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
