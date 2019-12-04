702ea3d5e3c3f4813605624e1baa08b5e4aa6e33
bushbass <alex.nielsen@gmail.com>
2019-12-04 12:16:36 -0500

    got edit user page working and linked to

--- src/components/user-list.component.js
+++ src/components/user-list.component.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import SingleUser from './single-user.component';
 
 function UsersList() {
   const [users, setUsers] = useState([]);
+
+  const deleteUser = id => {
+    axios
+      .delete('https://fcc-exercise-tracker-backend.herokuapp.com/users/' + id)
+      .then(response => {
+        console.log(response.data);
+      });
+    setUsers(users.filter(user => user._id !== id));
+  };
 
   useEffect(() => {
     axios
@@ -17,12 +27,23 @@ function UsersList() {
 
   return (
     <div>
-      <h2>All Users</h2>
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>{user.username}</li>
-        ))}
-      </ul>
+      <h3>All Users</h3>
+      <table className='table'>
+        <thead className='thead-light'>
+          <tr>
+            <th>Username</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            // <li key={user._id}>{user.username}</li>
+            <SingleUser user={user} key={user._id} deleteUser={deleteUser}>
+              test
+            </SingleUser>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
